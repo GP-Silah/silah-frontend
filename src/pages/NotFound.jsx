3f97c60d370f49cda43c8d9620ce5f5fb841ad43
gp2 <fairozzsagga@gmail.com>
2025-09-21 04:41:32 +0300

    save latest edits

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,26 +1,29 @@
 
 import { Link } from "react-router-dom";
+import GuestHeader from "../components/GuestHeader";
 import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    <main className="notfound">
-      <div className="notfound__inner">
-        <div className="notfound__code">404</div>
+    <>
+      <GuestHeader />
 
-        <h1 className="notfound__title">Oops!</h1>
-
-        <p className="notfound__lead">Looks like you took a wrong turn.</p>
-
-        <p className="notfound__text">
-          The page you’re looking for doesn’t exist or has been moved. Let’s get
-          you back on track!
-        </p>
-
-        <Link to="/" className="notfound__btn">
-          Return to Homepage
-        </Link>
-      </div>
-    </main>
+      <main className="notfound">
+        <div className="notfound__inner notfound__inner--withHeader">
+          <div className="notfound__code">404</div>
+          <h1 className="notfound__title">Oops!</h1>
+          <h2 className="notfound__subtitle">
+            Looks like you took a wrong turn.
+          </h2>
+          <p className="notfound__text">
+            The page you’re looking for doesn’t exist or has been moved. Let’s
+            get you back on track!
+          </p>
+          <Link to="/" className="notfound__button">
+            Return to Homepage
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
