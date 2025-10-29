8589cfd3081e548566814d8f875bb36bafc3777e
SHAHAD SAAD <ShahadSaad001.dev@gmail.com>
2025-10-29 07:41:50 +0300

    temp

--- src/layouts/BuyerLayout.jsx
+++ src/layouts/BuyerLayout.jsx
@@ -4,8 +4,10 @@ import Footer from '@/components/Footer/Footer';
 import DemoBanner from '@/components/DemoBanner/DemoBanner';
 import { Outlet } from 'react-router-dom';
 import { useNotifications } from '../hooks/useNotifications';
+import { useTranslation } from 'react-i18next';
 
 export default function BuyerLayout() {
+  const { i18n } = useTranslation();
   const { notifications } = useNotifications(i18n.language);
   const unread = notifications.filter((n) => !n.isRead).length;
 
